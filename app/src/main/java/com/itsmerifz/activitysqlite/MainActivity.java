05c91a3002f656c9766ba3fd840fd3c71a0455a8
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2021-04-20 22:59:10 +0800

    Initial commit

---
+++ app/src/main/java/com/itsmerifz/activitysqlite/MainActivity.java
@@ -0,0 +1,62 @@
+package com.itsmerifz.activitysqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.itsmerifz.activitysqlite.adapter.TemanAdapter;
+import com.itsmerifz.activitysqlite.database.DBController;
+import com.itsmerifz.activitysqlite.database.Teman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    private TemanAdapter adapter;
+    private ArrayList<Teman> temanArrayList;
+    private FloatingActionButton fab;
+    DBController dbc = new DBController(this);
+    String id,nm,tlp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rv = findViewById(R.id.recyclerView);
+        fab = findViewById(R.id.floatingBtn);
+        readData();
+        adapter = new TemanAdapter(temanArrayList);
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+        rv.setLayoutManager(lm);
+        rv.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,TemanBaru.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void readData(){
+        ArrayList<HashMap<String,String>> listTeman = dbc.getAllTeman();
+        temanArrayList = new ArrayList<>();
+//        Pindah hasil query
+        for (int i = 0; i < listTeman.size(); i++){
+            Teman t = new Teman();
+            t.setId(listTeman.get(i).get("id").toString());
+            t.setNama(listTeman.get(i).get("nama").toString());
+            t.setTelp(listTeman.get(i).get("telp").toString());
+//            Masukkan ke arrayList
+            temanArrayList.add(t);
+        }
+    }
+}
